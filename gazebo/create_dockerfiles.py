edf75217c47437496bb7f4e7e837de3eaeddd40e
ruffsl <roxfoxpox@gmail.com>
2015-05-15 13:51:13 -0700

    Moving parsing and ordered loading to ros_buildfarm.docker_common

--- gazebo/create_dockerfiles.py
+++ gazebo/create_dockerfiles.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 import pkg_resources
 import sys
 import yaml
 
-from collections import OrderedDict
 try:
     from cStringIO import StringIO
 except ImportError:
@@ -14,67 +12,26 @@ except ImportError:
 from em import Interpreter
 
 from ros_buildfarm.templates import create_dockerfile
-from ros_buildfarm.common import get_debian_package_name
 from ros_buildfarm.docker_common import DockerfileArgParser
-
-
-def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-    """Load yaml data into an OrderedDict"""
-    class OrderedLoader(Loader):
-        pass
-
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return object_pairs_hook(loader.construct_pairs(node))
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
-    return yaml.load(stream, OrderedLoader)
+from ros_buildfarm.docker_common import OrderedLoad
 
 
 def main(argv=sys.argv[1:]):
     """Create Dockerfiles for images from platform and image yaml data"""
 
-    # create the top-level parser
-    parser = argparse.ArgumentParser(
+    # Create the top-level parser
+    parser = DockerfileArgParser(
         description="Generate the 'Dockerfile's for the base docker images")
-    subparsers = parser.add_subparsers(
-                                       help='help for subcommand',
-                                       dest='subparser_name')
-
-    # create the parser for the "explicit" command
-    parser_explicit = subparsers.add_parser(
-        'explicit',
-        help='explicit --help')
-    parser_explicit.add_argument(
-        '-p', '--platform',
-        required=True,
-        help="Path to platform config")
-    parser_explicit.add_argument(
-        '-i', '--images',
-        required=True,
-        help="Path to images config")
-    parser_explicit.add_argument(
-        '-o', '--output',
-        required=True,
-        help="Path to write generate Dockerfiles")
-
-    # create the parser for the "dir" command
-    parser_dir = subparsers.add_parser(
-        'dir',
-        help='dir --help')
-    parser_dir.add_argument(
-        '-d', '--directory',
-        required=True,
-        help="Path to read config and write output")
-
+    parser.set()
     args = parser.parse_args(argv)
 
+    # If paths were given explicitly
     if args.subparser_name == 'explicit':
         platform_path = args.platform
         images_path = args.images
         output_path = args.output
 
+    # Else just use the given directory path
     elif args.subparser_name == 'dir':
         platform_path = 'platform.yaml'
         images_path = 'images.yaml.em'
@@ -82,7 +39,7 @@ def main(argv=sys.argv[1:]):
         images_path = os.path.join(args.directory, images_path)
         output_path = args.directory
 
-    # Ream platform perams
+    # Read platform perams
     with open(platform_path, 'r') as f:
         # use safe_load instead load
         platform = yaml.safe_load(f)['platform']
@@ -99,8 +56,8 @@ def main(argv=sys.argv[1:]):
     finally:
         interpreter.shutdown()
         interpreter = None
-    # Use ordered list
-    images = ordered_load(images_yaml, yaml.SafeLoader)['images']
+    # Use ordered dict
+    images = OrderedLoad(images_yaml, yaml.SafeLoader)['images']
 
     # For each image tag
     for image in images:
