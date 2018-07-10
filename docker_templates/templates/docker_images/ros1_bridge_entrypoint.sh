7d197c59d87277b0fa9d50b947659edf02611861
Mikael Arguedas <mikael@osrfoundation.org>
2018-07-10 10:35:50 -0700

    use ROS_DISTRO for ROS 2 and ROS1_distro for ROS 1

--- docker_templates/templates/docker_images/ros1_bridge_entrypoint.sh
+++ docker_templates/templates/docker_images/ros1_bridge_entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # setup ros1 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/opt/ros/$ROS1_DISTRO/setup.bash"
 
 # setup ros2 environment
-source "/opt/ros/$ROS2_DISTRO/setup.bash"
+source "/opt/ros/$ROS_DISTRO/setup.bash"
 exec "$@"
