ceffd9ff1e5ee64ba577fa162f2f96273d250973
Mikael Arguedas <mikael@osrfoundation.org>
2018-09-11 13:03:30 -0700

    unset rosdistro before sourcing to avoid spurious warning, also unset ROS1/2_DISTRO to leave an environment similar to native install

--- docker_templates/templates/docker_images/ros1_bridge_entrypoint.sh
+++ docker_templates/templates/docker_images/ros1_bridge_entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+unset ROS_DISTRO
 # setup ros1 environment
 source "/opt/ros/$ROS1_DISTRO/setup.bash"
 
 # setup ros2 environment
 source "/opt/ros/$ROS2_DISTRO/setup.bash"
+
+unset ROS1_DISTRO
+unset ROS2_DISTRO
 
 exec "$@"
