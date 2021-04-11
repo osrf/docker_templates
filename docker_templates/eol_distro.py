7c451bbb128353c27115fca741ee56020a0255cf
Mikael Arguedas <mikael.arguedas@gmail.com>
2021-04-11 08:28:10 +0200

    add stretch to EOL distro list
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- docker_templates/eol_distro.py
+++ docker_templates/eol_distro.py
@@ -52,5 +52,6 @@ def isDistroEOL(*, ros_distro_name=None, os_distro_name=None):
         # Debian
         'wheezy',
         'jessie',
+        'stretch',
     ]
     return os_distro_name in eol_base_images or ros_distro_name in eol_ros_distros
